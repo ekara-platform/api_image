978f1d614c543439e63e8ecdbefe1db7d6ee9992
nirekin <nirekin@hotmail.com>
2018-07-20 14:54:56 +0200

    Add docker-compose

--- copydep.sh
+++ copydep.sh
@@ -12,4 +12,7 @@ cp ../engine/ssh/*.go  ./go/vendor/github.com/lagoon-platform/engine/ssh
 # Refresh "api"
 rm -rf ./go/vendor/github.com/lagoon-platform/api/*.go
 cp ../api/*.go  ./go/vendor/github.com/lagoon-platform/api/
+rm -rf ./go/vendor/github.com/lagoon-platform/api/consul/*.go
+cp ../api/consul/*.go  ./go/vendor/github.com/lagoon-platform/api/consul
+
 
