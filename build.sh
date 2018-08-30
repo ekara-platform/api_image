8b8d55ff0e330744612f0e9ac19e89f1e9354c81
nirekin <nirekin@hotmail.com>
2018-08-30 16:56:22 +0200

    Add storage with consul

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 
 docker run --rm -v "$PWD/go":/go/src/installer -w /go/src/installer iron/go:dev go build -o api
 
-docker build -t lagoon-platform/api .     
+docker build -t lagoonplatform/api .     
